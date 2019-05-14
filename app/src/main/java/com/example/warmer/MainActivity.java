7ef2aeaf9460b204fad5957ff4afe0986c6f1e6f
hanjjm <hanjjm1994@naver.com>
2019-05-14 21:20:36 +0900

    add body, pad images

--- app/src/main/java/com/example/warmer/MainActivity.java
+++ app/src/main/java/com/example/warmer/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.warmer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+
 }
