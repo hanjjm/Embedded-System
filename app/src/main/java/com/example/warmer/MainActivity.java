261628fbd3dd0b2f0e78bac2dfa4f7f51a2c3abb
hanjjm <hanjjm1994@naver.com>
2019-05-15 21:57:26 +0900

    add button click

--- app/src/main/java/com/example/warmer/MainActivity.java
+++ app/src/main/java/com/example/warmer/MainActivity.java
@@ -2,9 +2,10 @@ package com.example.warmer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TabHost;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +13,55 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabHost tabHost1 = (TabHost) findViewById(R.id.tabHost1) ;
+        tabHost1.setup() ;
+
+        // 첫 번째 Tab. (탭 표시 텍스트:"TAB 1"), (페이지 뷰:"content1")
+        TabHost.TabSpec ts1 = tabHost1.newTabSpec("Tab Spec 1") ;
+        ts1.setContent(R.id.content1) ;
+        ts1.setIndicator("온도조절") ;
+        tabHost1.addTab(ts1)  ;
+
+        Button pad1 = (Button) findViewById(R.id.button);
+        pad1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "오른쪽 어꺠 온도 up~", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+        // 두 번째 Tab. (탭 표시 텍스트:"TAB 2"), (페이지 뷰:"content2")
+        TabHost.TabSpec ts2 = tabHost1.newTabSpec("Tab Spec 2") ;
+        ts2.setContent(R.id.content2) ;
+        ts2.setIndicator("온도체크") ;
+        tabHost1.addTab(ts2) ;
+
+        Button pad2 = (Button) findViewById(R.id.button2);
+        pad2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "왼쪽 어꺠 온도 up~", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        // 세 번째 Tab. (탭 표시 텍스트:"TAB 3"), (페이지 뷰:"content3")
+        TabHost.TabSpec ts3 = tabHost1.newTabSpec("Tab Spec 3") ;
+        ts3.setContent(R.id.content3) ;
+        ts3.setIndicator("켜기/끄기") ;
+        tabHost1.addTab(ts3) ;
+
+        Button pad3 = (Button) findViewById(R.id.button3);
+        pad3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "허리 온도 up~", Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
     }
 
